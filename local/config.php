806a6bc88b6ebfc3d29c63f7b4e3dab863d80ec6
Luke Fraser <totalvideopro@gmail.com>
2014-08-06 00:18:54 -0700

    adding image logo

--- local/config.php
+++ local/config.php
@@ -27,7 +27,7 @@ $EnablePathInfo = 1;
 
 ## $PageLogoUrl is the URL for a logo image -- you can change this
 ## to your own logo if you wish.
-$PageLogoUrl = "$PubDirUrl/skins/pmwiki/pmwiki-32.gif";
+$PageLogoUrl = "/flirteylogo.png";
 
 ## If you want to have a custom skin, then set $Skin to the name
 ## of the directory (in pub/skins/) that contains your skin files.
